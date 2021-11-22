6ebf7f6466f7d93d48129e629e283640ec10b36a
Redshark61 <tim.vannier@gmail.com>
2021-11-22 14:35:32 +0100

    change transparancy color

--- assets/Winter.tsx
+++ assets/Winter.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="Winter" tilewidth="16" tileheight="16" tilecount="8" columns="8">
- <image source="Ground/Winter.png" trans="003039" width="128" height="16"/>
+ <image source="Ground/Winter.png" trans="000000" width="128" height="16"/>
 </tileset>
