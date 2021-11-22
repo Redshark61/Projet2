e733b33ce797c07e5ff6fbb2e2b8b5cd5cd5e49a
Alpharius61 <91472214+Alpharius61@users.noreply.github.com>
2021-11-22 22:09:00 +0100

    Add files via upload

--- assets/Cliff-Lava.tsx
+++ assets/Cliff-Lava.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="Cliff-Lava" tilewidth="16" tileheight="16" tilecount="30" columns="5">
- <image source="assets/Ground/Cliff-Lava.png" trans="003039" width="80" height="96"/>
+ <image source="Ground/Cliff-Lava.png" trans="003039" width="80" height="96"/>
 </tileset>
--- assets/FireMausoleum.tsx
+++ assets/FireMausoleum.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="FireMausoleum" tilewidth="16" tileheight="16" tilecount="72" columns="12">
- <image source="assets/Buildings/Enemy/FireMausoleum.png" trans="000000" width="195" height="98"/>
+ <image source="Buildings/Enemy/FireMausoleum.png" trans="000000" width="195" height="98"/>
 </tileset>
