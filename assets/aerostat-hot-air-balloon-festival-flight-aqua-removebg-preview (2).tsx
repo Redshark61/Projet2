ce1a8e7b3e45644a3dce701d22f297c42be989d9
Redshark61 <tim.vannier@gmail.com>
2021-11-22 14:39:58 +0100

    added photo

--- assets/aerostat-hot-air-balloon-festival-flight-aqua-removebg-preview (2).tsx
+++ assets/aerostat-hot-air-balloon-festival-flight-aqua-removebg-preview (2).tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="aerostat-hot-air-balloon-festival-flight-aqua-removebg-preview (2)" tilewidth="16" tileheight="16" tilecount="4" columns="2">
- <image source="C:/Users/Timothée/Downloads/aerostat-hot-air-balloon-festival-flight-aqua-removebg-preview (2).png" trans="000000" width="32" height="32"/>
+ <image source="aerostat-hot-air-balloon-festival-flight-aqua-removebg-preview (2).png" trans="000000" width="32" height="32"/>
 </tileset>
