f1080a583f11668082a935265472e5f9841e496e
Alpharius61 <91472214+Alpharius61@users.noreply.github.com>
2021-11-22 21:49:23 +0100

    Add files via upload

---
+++ assets/Boat.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Boat" tilewidth="16" tileheight="16" tilecount="48" columns="12">
+ <image source="assets/Miscellaneous/TransportShipLavaZone.png" trans="003039" width="200" height="67"/>
+</tileset>
---
+++ assets/Staires.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Staires" tilewidth="16" tileheight="16" tilecount="63" columns="7">
+ <image source="assets/Ground/Cliff-fireZone.png" trans="003039" width="112" height="144"/>
+</tileset>
