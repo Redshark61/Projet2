b7dde6988ac603f19c39775d97e3e6ca02e79ce5
Redshark61 <tim.vannier@gmail.com>
2021-11-24 15:22:28 +0100

    working teleportation to worlds

--- map.py
+++ map.py
@@ -3,6 +3,8 @@ import pytmx
 import pyscroll
 import pygame
 
+from player import NPC
+
 
 @dataclass
 class Portal:
@@ -19,54 +21,90 @@ class Map:
     group: pyscroll.PyscrollGroup
     tmxData: pytmx.TiledMap
     portals: list[Portal]
+    npcs: list[NPC]
 
 
 class MapManager:
 
-    def __init__(self, game, entity: list):
+    def __init__(self, game):
         self.game = game
+        self.player = self.game.player
+        self.screen = self.game.screen
         self.maps = {}
         # Load the map
         self.currentMap = "assetHub/carte_hub_p2"
-        self.tmxData = pytmx.load_pygame("./assets/assetHub/carte_hub_p2.tmx")
-        mapData = pyscroll.data.TiledMapData(self.tmxData)
-        mapLayer = pyscroll.BufferedRenderer(mapData, game.screen.get_size())
-        mapLayer.zoom = 2
-
-        self.group = pyscroll.PyscrollGroup(map_layer=mapLayer, default_layer=18)
-        for i in entity:
-            self.group.add(i)
+        self.registerMap("assetHub/carte_hub_p2",
+                         portals=[
+                             Portal("assetHub/carte_hub_p2", "assetAir/airWorld", "toAir", "spawnPlayer")
+                         ],
+                         entity=[NPC("Monsters/Demons/RedDemon")])
+        self.registerMap("assetAir/airWorld",
+                         portals=[
+                             Portal("assetAir/airWorld", "assetHub/carte_hub_p2", "toHub", "spawnPlayer")
+                         ],
+                         entity=[NPC("Monsters/Demons/RedDemon")])
+        self.teleportNPC("spawnBoss")
 
     def checkCollision(self):
-        if self.currentMap == Portal.fromWorld:
-            pass
+        for portal in self.getMap().portals:
+            if self.currentMap == portal.fromWorld:
+                point = self.getObject(portal.originPoint)
+                rect = pygame.Rect(point.x, point.y, point.width, point.height)
+
+                if self.player.rect.colliderect(rect):
+                    copyPortal = portal
+                    self.currentMap = portal.toWorld
+                    self.teleportPlayer(copyPortal.destinationPoint)
+
+    def getMap(self):
+        return self.maps[self.currentMap]
+
+    def getGroup(self):
+        return self.getMap().group
 
     def drawMap(self):
-        self.group.draw(self.game.screen)
-        self.group.center(self.game.player.rect.center)
+        self.getGroup().draw(self.game.screen)
+        self.getGroup().center(self.game.player.rect.center)
 
     def updateMap(self):
-        self.group.update()
+        self.getGroup().update()
+        self.checkCollision()
+        for npc in self.getMap().npcs:
+            npc.move(self.player)
 
-    def teleportPlayer(self, objectName, entity):
-        # Get the tmx obejct with the name "playerSpawn"
-        spawn = self.tmxData.get_object_by_name(objectName)
-        # Set the player's position to the spawn point
-        entity.rect.x = spawn.x
-        entity.rect.y = spawn.y
+    def teleportPlayer(self, destinationName):
+        point = self.getObject(destinationName)
+        self.player.rect.x = point.x
+        self.player.rect.y = point.y
+        self.player.saveLocation()
 
-    def registerMap(self, mapName, portals):
+    def registerMap(self, mapName, portals, entity):
         tmxData = pytmx.util_pygame.load_pygame(f"./assets/{mapName}.tmx")
         mapData = pyscroll.data.TiledMapData(tmxData)
         mapLayer = pyscroll.orthographic.BufferedRenderer(mapData, self.screen.get_size())
         mapLayer.zoom = 2
 
         walls = []
-        for object in self.tmxData.objects:
+        for object in tmxData.objects:
             if object.name == "collision":
                 walls.append(pygame.Rect(object.x, object.y, object.width, object.height))
 
-        group = pyscroll.PyscrollGroup(map_layer=mapLayer, default_layer=5)
+        group = pyscroll.PyscrollGroup(map_layer=mapLayer, default_layer=10)
         group.add(self.player)
 
-        self.maps[mapName] = Map(mapName, walls, group, self.tmxData, portals)
+        for i in entity:
+            group.add(i)
+
+        self.maps[mapName] = Map(mapName, walls, group, tmxData, portals, entity)
+
+    def getObject(self, name):
+        return self.getMap().tmxData.get_object_by_name(name)
+
+    def teleportNPC(self, spawnName):
+        point = self.getObject(spawnName)
+        for map in self.maps:
+            mapData = self.maps[map]
+            npcs = mapData.npcs
+
+        for npc in npcs:
+            npc.teleportSpawn(point)
