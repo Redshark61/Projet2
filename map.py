d742b45f4095acca2868ddc2a65ff0c5c1f222b7
Redshark61 <tim.vannier@gmail.com>
2021-11-25 13:07:59 +0100

    added comments

--- map.py
+++ map.py
@@ -27,40 +27,28 @@ class Map:
 class MapManager:
 
     def __init__(self, game, screen):
+        # Initalize the variables
         self.game = game
         self.player = self.game.player
         self.screen = screen
         self.maps = {}
-        # Load the map
+        # Set the current map
         self.currentMap = "assetHub/carte_hub_p2"
 
-        self.registerMap("assetHub/carte_hub_p2",
-                         portals=[
-                             Portal("assetHub/carte_hub_p2", "assetAir/airWorld", "toAir", "spawnPlayer"),
-                             Portal("assetHub/carte_hub_p2", "assetTerre/mapTerre", "toTerre", "spawnPlayer"),
-                             Portal("assetHub/carte_hub_p2", "assetFeu/Fire_zone", "toFeu", "spawnPlayer"),
-                             Portal("assetHub/carte_hub_p2", "assetWater/WaterWorld", "toEau", "spawnPlayer"),
-                         ],
-                         entity=[NPC("Monsters/Demons/RedDemon")])
-
-        self.registerMap("assetAir/airWorld",
-                         portals=[
-                             Portal("assetAir/airWorld", "assetHub/carte_hub_p2", "toHub", "spawnPlayer")
-                         ],
-                         entity=[NPC("Monsters/Demons/RedDemon")])
-
-        self.registerMap("assetTerre/mapTerre",
-                         portals=[
-                             Portal("assetTerre/mapTerre", "assetHub/carte_hub_p2", "toHub", "spawnPlayer")
-                         ],
-                         entity=[NPC("Monsters/Demons/RedDemon")])
+        # Load all the maps
+        self.registerMap("assetHub/carte_hub_p2", portals=[Portal("assetHub/carte_hub_p2", "assetAir/airWorld", "toAir", "spawnPlayer"), Portal("assetHub/carte_hub_p2", "assetTerre/mapTerre", "toTerre", "spawnPlayer"), Portal(
+            "assetHub/carte_hub_p2", "assetFeu/Fire_zone", "toFeu", "spawnPlayer"), Portal("assetHub/carte_hub_p2", "assetWater/WaterWorld", "toEau", "spawnPlayer"), ], entity=[NPC("Monsters/Demons/RedDemon")])
+        self.registerMap("assetAir/airWorld", portals=[Portal("assetAir/airWorld", "assetHub/carte_hub_p2", "toHub", "spawnPlayer")], entity=[NPC("Monsters/Demons/RedDemon")])
+        self.registerMap("assetTerre/mapTerre", portals=[Portal("assetTerre/mapTerre", "assetHub/carte_hub_p2", "toHub", "spawnPlayer")], entity=[NPC("Monsters/Demons/RedDemon")])
         self.registerMap("assetFeu/Fire_zone", portals=[Portal("assetFeu/Fire_zone", "assetHub/carte_hub_p2", "toHub", "spawnPlayer")], entity=[NPC("Monsters/Demons/RedDemon")])
         self.registerMap("assetWater/WaterWorld", portals=[Portal("assetWater/WaterWorld", "assetHub/carte_hub_p2", "toHub", "spawnPlayer")], entity=[NPC("Monsters/Demons/RedDemon")])
 
         self.teleportNPC("spawnBoss")
 
     def checkCollision(self):
+        # Loop over all the portals
         for portal in self.getMap().portals:
+            # If the portal start from the current map
             if self.currentMap == portal.fromWorld:
                 point = self.getObject(portal.originPoint)
                 rect = pygame.Rect(point.x, point.y, point.width, point.height)
@@ -71,7 +59,6 @@ class MapManager:
                     self.teleportPlayer(copyPortal.destinationPoint)
 
         if self.player.feet.collidelist(self.getMap().walls) > -1:
-            print("collided with walls")
             self.player.rect.topleft = self.player.oldPosition
 
     def getMap(self):
@@ -99,8 +86,6 @@ class MapManager:
     def registerMap(self, mapName, portals, entity):
         tmxData = pytmx.util_pygame.load_pygame(f"./assets/{mapName}.tmx")
         mapData = pyscroll.data.TiledMapData(tmxData)
-        # print(type(mapData))
-        print(self.screen.get_size())
         mapLayer = pyscroll.orthographic.BufferedRenderer(mapData, self.screen.get_size())
         mapLayer.zoom = 2
 
