8045c00a2bee891d080ce4e2cb18285599375316
Redshark61 <tim.vannier@gmail.com>
2021-11-25 14:09:56 +0100

    working map !!

--- assets/assetFeu/Boat.tsx
+++ assets/assetFeu/Boat.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="Boat" tilewidth="16" tileheight="16" tilecount="48" columns="12">
- <image source="../Miscellaneous/TransportShipLavaZone.png" trans="003039" width="200" height="67"/>
+ <image source="../Miscellaneous/TransportShipLavaZone.png" trans="000000" width="200" height="67"/>
 </tileset>
