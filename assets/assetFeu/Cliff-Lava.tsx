a05124f23c836683f1874e9835a681bbc61e5b45
Alpharius61 <monlouis.gael@gmail.com>
2021-11-25 11:48:49 +0100

    Fire Zone ok

--- assets/assetFeu/Cliff-Lava.tsx
+++ assets/assetFeu/Cliff-Lava.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Cliff-Lava" tilewidth="16" tileheight="16" tilecount="120" columns="10">
- <image source="Cliff-Lava.png" trans="000000" width="160" height="192"/>
+<tileset version="1.5" tiledversion="1.7.2" name="Cliff-Lava" tilewidth="16" tileheight="16" tilecount="30" columns="5">
+ <image source="../Ground/Cliff-Lava.png" trans="003039" width="80" height="96"/>
 </tileset>
--- assets/assetFeu/Village.tsx
+++ assets/assetFeu/Village.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="Village" tilewidth="16" tileheight="16" tilecount="5" columns="5">
- <image source="../Buildings/Red/RedHuts.png" trans="000000" width="80" height="16"/>
+ <image source="../Buildings/Red/RedHuts.png" trans="003039" width="80" height="16"/>
 </tileset>
