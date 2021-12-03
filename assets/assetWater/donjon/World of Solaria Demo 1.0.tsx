8fa5def7503f4f3c6b6cdaef279c1a9d88429470
lordhiraishin <lemardeley.quentin.pro@hotmail.fr>
2021-12-03 13:46:59 +0100

    vrai de vrai promis

--- assets/assetWater/donjon/World of Solaria Demo 1.0.tsx
+++ assets/assetWater/donjon/World of Solaria Demo 1.0.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="World of Solaria Demo 1.0" tilewidth="16" tileheight="16" tilecount="476" columns="28">
- <image source="../../../../../Downloads/Solaria Demo Pack/Solaria Demo Pack/World of Solaria Demo 1.0.png" trans="000000" width="448" height="272"/>
+ <image source="assetWater/donjon/Solaria Demo Pack/World of Solaria Demo 1.0.png" trans="000000" width="448" height="272"/>
 </tileset>
