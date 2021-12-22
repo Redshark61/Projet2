fd2f5515ded8eee473cc5764938f583a168a36e1
Redshark61 <tim.vannier@gmail.com>
2021-12-22 09:16:29 +0100

    add blue filter

--- FinalNight.py
+++ FinalNight.py
@@ -1,7 +1,8 @@
 import time
+import pygame
 from db.db import Database
 from db.playerData import PlayerData
-import Variables
+import variables
 
 
 class Night:
@@ -13,6 +14,7 @@ class Night:
     healthModificator = 0
     damageModificator = 0
     timer = False
+    alpha = 0
 
     @classmethod
     def dataRecovery(cls):
@@ -44,22 +46,47 @@ class Night:
 
         if time.time() > cls.periodTime:
             cls.periodTime = time.time() + cls.dayLength[0][0]
-            Variables.Night = not Variables.Night
-            print(f"Night: {Variables.Night}")
+            variables.night = not variables.night
+            print(f"Night: {variables.night}")
             cls.monsterStatsModified = False
 
-        if Variables.Night and not cls.monsterStatsModified:
-            for _, npc in enumerate(Variables.map.getMap().npcs):
+        if variables.night and not cls.monsterStatsModified:
+            for _, npc in enumerate(variables.map.getMap().npcs):
                 npc.health = npc.health*cls.healthModificator
                 npc.maxHealth = npc.maxHealth*cls.healthModificator
                 npc.monsterDamage = npc.monsterDamage*cls.damageModificator
                 npc.xp = npc.xp*cls.xpModificator
             cls.monsterStatsModified = True
 
-        if not Variables.Night and not cls.monsterStatsModified:
-            for _, npc in enumerate(Variables.map.getMap().npcs):
+        if not variables.night and not cls.monsterStatsModified:
+            for _, npc in enumerate(variables.map.getMap().npcs):
                 npc.maxHealth = npc.maxHealth/cls.healthModificator
                 npc.health = npc.health/cls.healthModificator
                 npc.monsterDamage = npc.monsterDamage/cls.damageModificator
                 npc.xp = npc.xp/cls.xpModificator
             cls.monsterStatsModified = True
+
+    @classmethod
+    def drawBlueFilter(cls):
+        """
+        Draw blue filter on the screen
+        """
+
+        # Create a blue surface in order to apply transparency
+        blueFilter = pygame.Surface((variables.screen.get_width(), variables.screen.get_width()))
+        # By default the filter is transparent
+        blueFilter.fill((13, 1, 138))
+        blueFilter.set_alpha(cls.alpha)
+        # The ratio is how much we need to add to the alpha value in the time of the period
+        ratio = 150/cls.dayLength[0][0] / 3
+
+        # If it's the night
+        if variables.night:
+            # Add opacity to the filter
+            cls.alpha += ratio if cls.alpha < 150 else 0
+            blueFilter.set_alpha(cls.alpha)
+            variables.screen.blit(blueFilter, (0, 0))
+        else:
+            cls.alpha -= ratio if cls.alpha > 0 else 0
+            blueFilter.set_alpha(cls.alpha)
+            variables.screen.blit(blueFilter, (0, 0))
